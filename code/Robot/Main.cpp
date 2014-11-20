3658a9ffed0dbf54e7a1235be71380a6f2590675
Romain GUILLOT <guillot.romain@etu.unice.fr>
2014-11-20 16:38:10 +0100

    Main et afficheur

--- code/Robot/Main.cpp
+++ code/Robot/Main.cpp
@@ -2,6 +2,7 @@
 #include "Objet.h"
 #include "Afficheur.h"
 #include "Robot.h"
+#include <stdio>
 
 using namespace std;
 
@@ -9,10 +10,35 @@ int main(int argc, char *argv[])
 { 
 	Afficheur afficheur;
 	Robot robot;
-	afficheur.set_observable(robot);
+	afficheur.setObservable(robot);
+	robot.setObserveur(afficheur);
+	Objet o1(2);
+	Plot p1(20, o1);
+	Plot p2(35, NULL);
+
+	/* cas nominal */
 	robot.tourner('E');
 	robot.avancer(3);
 	robot.tourner('N');
 	robot.avancer(1);
+	robot.rencontrerPlot(p1);
+	cout << "Taille du plot : " << robot.evaluerPlot();
+	robot.saisir();
+	cout << " Poids de l'objet : " << robot.peser() << endl;
+	robot.tourner('S');
+	robot.avancer(1);
+	robot.peser();
+	robot.avancer(1);
+	robot.figer();
+	robot.repartir();
+	robot.rencontrerPlot(p2);
+	robot.poser();
+	robot.tourner('O');
+	robot.avancer(3);
+	robot.tourner('S');
+	robot.avancer(1);
+
+	/* cas d'erreur */
+		
 	
 }
